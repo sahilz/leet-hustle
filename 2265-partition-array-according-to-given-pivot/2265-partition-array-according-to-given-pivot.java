00c5796724c2e2d0fc2dc85f2b3fa61bd379c068
Sahil Zainuddin <sahilzainuddin1408@gmail.com>
2025-03-03 16:48:18 +0530

    Time: 5 ms (85.39%), Space: 68.8 MB (5.53%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] res = new int[nums.length];
-        List<Integer> small = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> big = new ArrayList<>();
+        int left = 0;
+        int count = 0;
+
         for(int i : nums) {
             if(i < pivot) {
-                small.add(i);
-            } else if (i > pivot) {
-                big.add(i);
-            } else {
-                equal.add(i);
+                res[left++] = i;
+            } else if (i == pivot) {
+                count++;
             }
         }
-        res = IntStream.concat(
-                                  IntStream.concat(small.stream().mapToInt(i -> i), 
-                                                   equal.stream().mapToInt(i -> i)),
-                                  big.stream().mapToInt(i -> i))
-                              .toArray();
+        while(count-->0) {
+            res[left++] = pivot;
+        }
+        for(int i : nums) {
+            if(i > pivot) {
+                res[left++] = i;
+            }
+        }
         return res;
     }
 }
