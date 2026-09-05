131a552ca8750ced2cd9fc699ec0d44af39a245f
Sahil Zainuddin <sahilzainuddin1408@gmail.com>
2026-09-05 22:32:10 +0530

    Time: 6 ms (45.8%), Space: 126.8 MB (48.85%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        final int len = nums.length;
+        int[] prefMax = new int[len];
+        int[] suffMin = new int[len];
+        calculatePrefMax(prefMax, nums);
+        calculateSuffMin(suffMin, nums);
+        int res = -1;
+        for(int i = 0; i < len; i++) {
+            if(prefMax[i]-suffMin[i] <= k) {
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+    private void calculatePrefMax(int[] prefMax, int[] nums) {
+        prefMax[0] = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            prefMax[i] = Math.max(prefMax[i - 1], nums[i]);
+        }
+    }
+    private void calculateSuffMin(int[] suffMin, int[] nums) {
+        suffMin[nums.length - 1] = nums[nums.length - 1];
+        for(int i = nums.length - 2; i >= 0; i--) {
+            suffMin[i] = Math.min(suffMin[i + 1], nums[i]);
+        }
+    }
+}
